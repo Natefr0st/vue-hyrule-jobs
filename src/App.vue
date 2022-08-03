45833e2741a2b14515e66e1b463d0a58155e838a
Kevin <kevin_zino@yahoo.com>
2022-08-03 13:31:16 +0300

    Created new JobList component, introduced a new directory with ts types/interfaces, job list styles

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="app">
-    Hello Hyrule
+    <JobList :jobs="jobs"/>
   </div>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue';
+import {defineComponent, reactive, ref, toRefs} from 'vue';
+import {Job} from "@/types/Job";
+import JobList from "@/components/JobList.vue";
 
 export default defineComponent({
   name: 'App',
-  components: {}
+  components: {JobList},
+  setup() {
+    const jobs = ref<Job[]>([
+      {title: "farm worker", location: "lon lon ranch", salary: 30000, id: "1"},
+      {title: "quaryman", location: "death mountain", salary: 40000, id: "2"},
+      {title: "flute player", location: "the lost woods", salary: 35000, id: "3"},
+      {title: "fisherman", location: "lake hylia", salary: 21000, id: "4"},
+      {title: "prison guard", location: "gerudo valley", salary: 32000, id: "5"},
+    ])
+
+    return {jobs}
+  },
 });
 </script>
 
